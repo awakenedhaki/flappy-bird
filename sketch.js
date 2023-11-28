b4b4dd1f1b93262daf717c310f81edcbf87aaec3
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-11-28 00:58:04 -0800

    Declaring global variables and setting the canvas background

--- sketch.js
+++ sketch.js
@@ -1,4 +1,16 @@
-// p5js Boilerplate ============================================================
-function setup() {}
+// Declaring Global Variables ==================================================
+let bird;
+const pipes = [];
 
-function draw() {}
+// Constants ===================================================================
+const SCALE = 1.5;
+const ENGLISH_VIOLET = [76, 59, 77];
+
+// p5js Boilerplate ============================================================
+function setup() {
+  createCanvas(135 * SCALE, 203 * SCALE);
+}
+
+function draw() {
+  background(...ENGLISH_VIOLET);
+}
