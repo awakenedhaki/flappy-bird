48967cc6b5ed3d3f33244d11af2919f23fff707d
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-11-28 21:10:22 -0800

    Game render

--- sketch.js
+++ sketch.js
@@ -1,27 +1,64 @@
 // Declaring Global Variables ==================================================
-let bird;
-const pipes = [];
+let birds, pipes;
 
 // Constants ===================================================================
 const SCALE = 1.5;
 const ENGLISH_VIOLET = [76, 59, 77];
+const COLLISION_TOLERANCE = 0.65;
+const N_BIRDS = 50;
 
 // p5js Boilerplate ============================================================
 function setup() {
   createCanvas(135 * SCALE, 203 * SCALE);
 
-  bird = new Bird();
+  birds = new Population(N_BIRDS);
+  pipes = new Pipes();
 }
 
 function draw() {
   background(...ENGLISH_VIOLET);
 
-  bird.show();
-  bird.update();
+  // Players
+  birds.update();
+  birds.show();
+
+  // Obstactles
+  pipes.update();
+  pipes.show();
+
+  // Obstacle Management
+  pipes.removePipe();
+  pipes.spawnPipe();
+
+  // Game/Population Management
+  birds.forEach((bird, i) => {
+    if (collision(bird, pipes.firstPipe)) {
+      birds.cull(i);
+    }
+    inactivatePipe(bird, pipes.firstPipe);
+
+    bird.predict(pipes.firstPipe);
+  });
 }
 
-function keyPressed() {
-  if (key === " ") {
-    bird.flap();
+// Game Management =============================================================
+function collision(bird, pipe) {
+  if (!pipe.active) {
+    return false;
+  }
+  const hitBox = bird.toHitBox();
+  const opening = pipe.toOpening();
+
+  const xCollision = hitBox.right >= pipe.x;
+  const yCollision =
+    hitBox.top <= opening.top || hitBox.bottom >= opening.bottom;
+
+  return xCollision && yCollision;
+}
+
+function inactivatePipe(bird, pipe) {
+  const hitBox = bird.toHitBox();
+  if (hitBox.left >= pipe.rightEdge * COLLISION_TOLERANCE) {
+    pipe.inactivate();
   }
 }
