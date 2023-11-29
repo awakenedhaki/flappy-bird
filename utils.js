7354d2d0207a209863a689cbe682740b5ee8709a
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-11-28 21:54:59 -0800

    Fix typo in start value

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ function integerLinearSpace(start, stop, cardinality) {
   const linearSpace = [];
   const step = (stop - start) / (cardinality - 1);
   for (let i = 0; i < cardinality; i++) {
-    linearSpace.push(startValue + step * i);
+    linearSpace.push(start + step * i);
   }
   return linearSpace;
 }
