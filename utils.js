8bb250072a51c0bc7e656548fac2caa61a6a4f9b
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-11-29 08:57:09 -0800

    Add JSDoc documentation to weighted random selection

--- utils.js
+++ utils.js
@@ -1,3 +1,10 @@
+/**
+ * Selects an element randomly based on provided weights.
+ * @function weightedRandomSelection
+ * @param {any[]} elements - The array of elements to select from.
+ * @param {number[]} weights - The array of weights corresponding to elements.
+ * @returns {any|null} - Returns a randomly selected element or null if unsuccessful.
+ */
 function weightedRandomSelection(elements, weights) {
   const totalWeight = weights.reduce((acc, weight) => acc + weight, 0);
   const randomValue = Math.random() * totalWeight;
