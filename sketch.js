99966fa45bbbb54f267ad49c66001049d362e2b5
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-12-05 22:17:35 -0800

    Add documentation to game management functions

--- sketch.js
+++ sketch.js
@@ -61,6 +61,13 @@ function draw() {
 }
 
 // Game Management =============================================================
+/**
+ * Checks if a bird has collided with a pipe or the border.
+ * @function collision
+ * @param {Bird} bird - The bird to check for collisions.
+ * @param {Pipe} pipe - The pipe to check for collisions.
+ * @returns {boolean} - Returns true if a collision occurred, false otherwise.
+ */
 function collision(bird, pipe) {
   if (!pipe.active) {
     return false;
@@ -71,10 +78,23 @@ function collision(bird, pipe) {
   return pipeCollision(hitBox, pipeOpening) || borderCollision(hitBox);
 }
 
+/**
+ * Checks if a bird has collided with the border.
+ * @function borderCollision
+ * @param {Object} hitBox - The hitbox of the bird.
+ * @returns {boolean} - Returns true if a collision with the border occurred, false otherwise.
+ */
 function borderCollision(hitBox) {
   return hitBox.top <= 0 || hitBox.bottom >= height;
 }
 
+/**
+ * Checks if a bird has collided with a pipe.
+ * @function pipeCollision
+ * @param {Object} hitBox - The hitbox of the bird.
+ * @param {Object} pipeOpening - The opening of the pipe.
+ * @returns {boolean} - Returns true if a collision with the pipe occurred, false otherwise.
+ */
 function pipeCollision(hitBox, pipeOpening) {
   const xCollision = hitBox.right >= pipeOpening.left;
   const yCollision =
@@ -83,6 +103,12 @@ function pipeCollision(hitBox, pipeOpening) {
   return xCollision && yCollision;
 }
 
+/**
+ * Checks if a bird has crossed a pipe.
+ * @function pipeCrossed
+ * @param {Bird} bird - The bird to check.
+ * @param {Pipe} pipe - The pipe to check.
+ */
 function pipeCrossed(bird, pipe) {
   const hitBox = bird.toHitBox();
   if (hitBox.right >= pipe.leftEdge * CROSSING_THRESHOLD && pipe.active) {
@@ -90,6 +116,12 @@ function pipeCrossed(bird, pipe) {
   }
 }
 
+/**
+ * Deactivates a pipe if a bird has passed it.
+ * @function inactivatePipe
+ * @param {Bird} bird - The bird to check.
+ * @param {Pipe} pipe - The pipe to check.
+ */
 function inactivatePipe(bird, pipe) {
   const hitBox = bird.toHitBox();
   if (hitBox.left > pipe.rightEdge * COLLISION_TOLERANCE) {
